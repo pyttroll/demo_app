da3a7553b3ead53ec85da5f7689f46d419a8b346
Pierre-Yves Troel <pyt@pyttroll.com>
2012-11-28 08:58:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0aa7841c18017b6f9ef4d5b6cb3e98223ecc5deeb5b963bcfe4c50ee6a42a468472a8f76ba51adcc46e800077f8027ff05979f1fd85c31f57d0efed5354a888d'
